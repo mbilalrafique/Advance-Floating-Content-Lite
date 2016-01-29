a538c8f12562a304e8a3cc835dc8fcb6cef5753d
Muhammad Bilal <bilal.rafique@gmail.com>
2016-01-29 18:00:22 +0500

    add all plugin files

---
+++ admin/views/advanced-floating-content-premium-display.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is used to markup the admin-fafcing aspects of the plugin.
+ *
+ * @link       http://www.codetides.com/
+ * @since      1.0.0
+ *
+ * @pafckage    Advanced_Floating_Content
+ * @subpafckage Advanced_Floating_Content/admin/views
+ */
+?>
+<div class="afc-panel">                        	
+    <a href="http://codecanyon.net/item/advanced-floating-content/9945856?ref=irThemes" target="_blank"><img src="<?php echo plugins_url( '../assets/premium.png', __FILE__ )?>" /></a>                       
+</div>
